d81efcd8a1c0f57c93316e51297ce54b69d9afed
李小热 <freemart@126.com>
2013-12-08 23:41:35 +0800

    form

--- wms/src.wms/com/koomii/wms/controller/FormController.java
+++ wms/src.wms/com/koomii/wms/controller/FormController.java
@@ -1,12 +1,16 @@
 package com.koomii.wms.controller;
 
 import com.koomii.base.BaseController;
+import com.koomii.wms.model.Customer;
+import com.koomii.wms.model.Storage;
 
 import static com.koomii.wms.common.ModelConfigWms.*;
 
 public class FormController extends BaseController {
 	
 	public void io() {
+		setAttr("storageList", Storage.dao.find("select * from wms_storage"));
+		setAttr("customerList", Customer.dao.find("select * from wms_customer"));
 		render("../form_io.html");
 	}
 	
